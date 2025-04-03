d935c31c97d2a613f7f686ef39bffd6f05fb9a4e
Kyra Riedel <kyrariedel@gmail.com>
2025-04-02 22:13:16 -0400

    a5

---
+++ Kambaz/Database/grades.js
@@ -0,0 +1,6 @@
+export default [
+    { _id: "1", user: "123", course: "RS101", grade: "90"},  { _id: "2", user: "234", course: "RS101", grade: "90"},
+    { _id: "3", user: "345", course: "RS101", grade: "80" },  { _id: "4", user: "456", course: "RS101", grade: "70"},
+    { _id: "5", user: "567", course: "RS101", grade: "93" },  { _id: "6", user: "234", course: "RS102", grade: "85"},
+    { _id: "7", user: "789", course: "RS102", grade: "80" },  { _id: "8", user: "890", course: "RS102", grade: "95"},
+    { _id: "9", user: "123", course: "RS102", grade: "72" } ]
